6aa564d09ecb070e1bff49cee44dd4b4489371fc
zhongwen <zhongwen@zhongwendeMacBook-Pro.local>
2020-07-15 22:46:36 +0800

    导航栏和工具栏

---
+++ 导航栏和工具栏/导航栏和工具栏/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  导航栏和工具栏
+//
+//  Created by 仲雯 on 2020/7/15.
+//  Copyright © 2020 仲雯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
